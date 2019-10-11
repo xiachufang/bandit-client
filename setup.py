60f409a6240e4dc7c7cd202eaaa18236a6249ae3
x1ah <x1ahgxq@gmail.com>
2019-10-11 15:56:16 +0800

    bump version

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ from setuptools import find_packages, setup
 
 setup(
     name='bandit_client',
-    version='0.1.0',
+    version='0.2.0',
     keywords=('bandit client',),
     description='bandit client',
     license='MIT License',
